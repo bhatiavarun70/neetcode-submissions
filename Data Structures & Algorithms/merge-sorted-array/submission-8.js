2c05563215bfb6f01be9feee67e854813c586d6e
Varun Bhatia <bhatiavarun70@gmail.com>
2026-07-21 01:02:36 +0530

    Add: merge-sorted-array - submission-8

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-8.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        let temp = [];
+        let i = 0;
+        let j = 0;
+        if(m === 0){
+            for(let i = 0; i < nums1.length; i++){
+                nums1[i] = nums2[i]
+            }
+            
+        } else if(m > 0 && n > 0 ){
+            const tempArr1 = nums1.slice(0, m)
+            while(i < nums2.length && j < tempArr1.length){
+                if(nums2[i] < tempArr1[j]){
+                    temp.push(nums2[i])
+                    i++;
+                } else{
+                    temp.push(tempArr1[j])
+                    j++;
+                }
+            }
+            while(i < nums2.length){
+                temp.push(nums2[i])
+                i++
+            }
+            while(j < tempArr1.length){
+                temp.push(tempArr1[j])
+                j++
+            }
+            for(let i = 0; i < nums1.length; i++){
+                nums1[i] = temp[i]
+            }
+        }
+    }
+}
