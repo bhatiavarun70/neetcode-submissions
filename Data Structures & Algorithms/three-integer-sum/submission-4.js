9c0df3026d0fcc0699d49097f8f8ad1b7565665b
Varun Bhatia <bhatiavarun70@gmail.com>
2026-07-22 01:10:41 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let answer = [];
+        nums.sort((a, b) => a - b);
+        for(let i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] === nums[i - 1]) continue;
+            let j = i + 1;
+            let k = nums.length - 1;
+            while(j < k){
+                const sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++
+                } else if(sum > 0){
+                    k--;
+                } else{
+                    const temp = [nums[i], nums[j], nums[k]]
+                    answer.push(temp)
+                    j++;
+                    k--;
+                    while(j < k && nums[j] === nums[j - 1]) j++;
+                    while(j < k && nums[k] === nums[k + 1]) k--;
+                }
+            }
+        }
+        return answer
+    }
+}
