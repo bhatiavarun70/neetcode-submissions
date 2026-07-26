2abccd113bb2260e30bb2308974d61677db10b60
Varun Bhatia <bhatiavarun70@gmail.com>
2026-07-26 21:47:38 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        const scores = [];
+        for(let i = 0; i < operations.length; i++){
+            switch(operations[i]){
+                case "+" : {
+                    scores.push(Number(scores[scores.length - 1]) + Number(scores[scores.length - 2]));
+                    break;
+                }
+                case "C": {
+                    scores.pop();
+                    break
+                }
+                case "D": {
+                    scores.push(Number(scores[scores.length - 1]) * 2);
+                    break;
+                }
+                default: {
+                    scores.push(Number(operations[i]));
+                    break;
+                }
+            }
+        }
+        let total = 0;
+        scores.forEach(each => {
+            total += each
+        })
+        return total;
+    }
+}
