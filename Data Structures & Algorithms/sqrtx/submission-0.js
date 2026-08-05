93aaaf7f0d68947555cf956298b42073e047fae7
Varun Bhatia <bhatiavarun70@gmail.com>
2026-08-05 21:41:02 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    mySqrt(x) {
+        let left = 1;
+        let right = x;
+
+        while(left <= right){
+            let mid = Math.floor((left + right) / 2);
+            let square = mid * mid;
+            if(square === x){
+                return mid;
+            } else if(square > x){
+                right = mid - 1;
+            } else left = mid + 1;
+        }
+        if(left * left < x){
+            return left;
+        } else return left - 1;
+    }
+}
