638561f585b873e21b5f7c58fac578e26631769e
Varun Bhatia <bhatiavarun70@gmail.com>
2026-08-05 23:45:14 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let left = 1;
+  let right = Math.max(...piles);
+  while (left <= right) {
+    let k = Math.floor((left + right) / 2);
+    let hours = 0;
+    for (const pile of piles) {
+      hours += Math.ceil(pile / k);
+    }
+    if (hours > h) {
+      left = k + 1;
+    } else if (hours <= h) {
+      right = k - 1;
+    }
+  }
+  return left;
+    }
+}
